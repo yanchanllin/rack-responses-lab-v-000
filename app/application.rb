0bd09d4ecbbafb5e673bf81f2f04110a99d6cf30
YanC Lin <yonalin1@gmail.com>
2018-03-08 16:22:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ class Application
 
     if (0..11).include?(Time.now.hour)
       resp.write 'Good Morning!'
-  else
-     resp.write 'Good Afternoon!'
+    else
+      resp.write 'Good Afternoon!'
+    end
+    resp.finish
   end
-  resp.finish
- end
 end
